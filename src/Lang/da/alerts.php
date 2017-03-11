c39804ea1446085926ab8b6db963b6d94941df25
ThomasBS <ts@jumdum.com>
2017-03-11 14:39:58 +0100

    Add danish translations for post controller allerts

--- src/Lang/da/alerts.php
+++ src/Lang/da/alerts.php
@@ -1,4 +1,11 @@
 <?php
 return [
-    'spam_prevention_notice:mins' => 'For at undgå spam, vent venligst :mins minut før du opretter nyt indlæg/besked.'
+    'spam_prevention_notice:mins' => 'For at undgå spam, vent venligst :mins minut før du opretter nyt indlæg/besked.',
+    'response_submit_success:discussion' => 'Indlæg blev oprettet til :discussion.',
+    'response_submit_fail' => 'Beklager, det ser ud til at der var problemer med at oprette dit indlæg.',
+    'response_update_success:discussion' => 'Dit indlæg til :discussion blev opdateret.',
+    'response_update_fail' => 'Beklager, kunne ikke opdatere dit indlæg.',
+    'response_and_discussion_delete_success:discussion' => 'Dit indlæg samt :discussion blev slettet.',
+    'response_delete_success:discussion' => 'Dit indlæg blev slettet fra :discussion.',
+    'response_delete_fail' => 'Beklager, kunne ikke slette dit indlæg',
 ];

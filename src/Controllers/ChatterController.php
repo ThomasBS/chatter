8ffa07fd50c230c4965b9a73a44ce4096da76eb8
ThomasBS <ts@jumdum.com>
2017-08-31 16:43:38 +0200

    Order discussions fetched by updated_at

--- src/Controllers/ChatterController.php
+++ src/Controllers/ChatterController.php
@@ -12,11 +12,11 @@ class ChatterController extends Controller
     {
         $pagination_results = config('chatter.paginate.num_of_results');
 
-        $discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->orderBy('created_at', 'DESC')->paginate($pagination_results);
+        $discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->orderBy('updated_at', 'DESC')->paginate($pagination_results);
         if (isset($slug)) {
             $category = Models::category()->where('slug', '=', $slug)->first();
             if (isset($category->id)) {
-                $discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->where('chatter_category_id', '=', $category->id)->orderBy('created_at', 'DESC')->paginate($pagination_results);
+                $discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->where('chatter_category_id', '=', $category->id)->orderBy('updated_at', 'DESC')->paginate($pagination_results);
             }
         }
 
